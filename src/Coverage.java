eb1ebdcc12e9fb75048a3343ad36caf52d2d98e2
Catarina Costa <catarinasouzacosta@gmail.com>
2015-06-22 12:19:10 -0500

    Coverage test

---
+++ src/Coverage.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Catarina
+ */
+public class Coverage {
+	
+	public void a(){
+		
+	}
+	
+	public void b(){
+		
+	}
+	
+	public void c(){
+		
+	}
+	
+	public void d(){
+		
+	}
+	
+	public void e(){
+		
+	}
+}
